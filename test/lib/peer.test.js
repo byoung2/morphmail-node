ec12ebc35025809efa66bc2579a329c4e247fd39
Bryan Young <byoung2@gmail.com>
2022-02-10 16:50:20 -0800

    Improvement: adding IPv6 sorting for peers

--- test/lib/peer.test.js
+++ test/lib/peer.test.js
@@ -4,27 +4,78 @@ const assert = require('assert');
 
 describe('Peer', function() {
     describe('find closest', function() {
-        it('should find the closest peers by IP address', function() {
+        it('should find the closest peers by IPv4 address', function() {
             const testWallet = '1HUqrQ9gkyDmkxnJ1TwSi1AbTncAfGGthM';
             const ipAddresses = [
-                "36.146.80.237",
-                "186.143.177.136",
-                "125.227.154.208",
-                "16.66.45.157",
-                "88.48.48.31",
-                "156.112.70.192",
-                "249.153.115.133",
-                "137.16.160.250",
-                "66.196.171.158",
-                "144.91.68.187",
-                "186.231.103.124",
-                "117.140.43.64",
-                "106.28.186.115",
-                "71.29.52.2",
-                "26.196.7.217"
+                '36.146.80.237',
+                '186.143.177.136',
+                '125.227.154.208',
+                '16.66.45.157',
+                '88.48.48.31',
+                '156.112.70.192',
+                '249.153.115.133',
+                '137.16.160.250',
+                '66.196.171.158',
+                '144.91.68.187',
+                '186.231.103.124',
+                '117.140.43.64',
+                '106.28.186.115',
+                '71.29.52.2',
+                '26.196.7.217',
             ];
             const sortedIpAddresses = peer.getNearestPeers(testWallet, ipAddresses);
             assert.equal(sortedIpAddresses[0], '16.66.45.157');
+        });
+
+        it('should find the closest peers by IPv6 address', function() {
+            const testWallet = '1HUqrQ9gkyDmkxnJ1TwSi1AbTncAfGGthM';
+            const ipAddresses = [
+                'd23f:a82c:8950:032a:58c3:fe31:31db:9587',
+                'e4e9:995e:4780:cc12:9987:03e9:787e:62cb',
+                'ce0e:93a3:d11a:5e93:acb7:bc28:3ae1:beec',
+                'd274:8c1a:0585:6710:8fb3:13b7:35f7:49c9',
+                '2d7f:4059:2208:f578:a20f:06e0:a28c:db24',
+                'bf9a:9ed8:0290:66cc:e0e6:7af2:461b:ae2e',
+                '2bab:8f74:59e3:a77e:2a0d:da34:04bd:fab8',
+                'a50c:d9b5:c884:0962:96ba:f4f8:60b0:c4b7',
+                'bd8f:2757:4e5a:2458:b89f:992d:616f:42e8',
+                '7574:9791:27de:bd69:fe58:2e42:1d2a:26aa',
+            ];
+            const sortedIpAddresses = peer.getNearestPeers(testWallet, ipAddresses);
+            assert.equal(sortedIpAddresses[0], '2bab:8f74:59e3:a77e:2a0d:da34:04bd:fab8');
+        });
+
+        it('should find the closest peers by IPv4 and IPv6 addresses', function() {
+            const testWallet = '1HUqrQ9gkyDmkxnJ1TwSi1AbTncAfGGthM';
+            const ipAddresses = [
+                '36.146.80.237',
+                '186.143.177.136',
+                '125.227.154.208',
+                '16.66.45.157',
+                '88.48.48.31',
+                '156.112.70.192',
+                '249.153.115.133',
+                '137.16.160.250',
+                '66.196.171.158',
+                '144.91.68.187',
+                '186.231.103.124',
+                '117.140.43.64',
+                '106.28.186.115',
+                '71.29.52.2',
+                '26.196.7.217',
+                'd23f:a82c:8950:032a:58c3:fe31:31db:9587',
+                'e4e9:995e:4780:cc12:9987:03e9:787e:62cb',
+                'ce0e:93a3:d11a:5e93:acb7:bc28:3ae1:beec',
+                'd274:8c1a:0585:6710:8fb3:13b7:35f7:49c9',
+                '2d7f:4059:2208:f578:a20f:06e0:a28c:db24',
+                'bf9a:9ed8:0290:66cc:e0e6:7af2:461b:ae2e',
+                '2bab:8f74:59e3:a77e:2a0d:da34:04bd:fab8',
+                'a50c:d9b5:c884:0962:96ba:f4f8:60b0:c4b7',
+                'bd8f:2757:4e5a:2458:b89f:992d:616f:42e8',
+                '7574:9791:27de:bd69:fe58:2e42:1d2a:26aa',
+            ];
+            const sortedIpAddresses = peer.getNearestPeers(testWallet, ipAddresses);
+            assert.equal(sortedIpAddresses[0], '2bab:8f74:59e3:a77e:2a0d:da34:04bd:fab8');
         });
     });
 });
--- test/lib/util/ipAddress.test.js
+++ test/lib/util/ipAddress.test.js
@@ -3,22 +3,37 @@ const assert = require('assert');
 
 describe('IpAddress', function() {
     describe('constuctor', function() {
-        const string = '127.0.0.1';
-        const addressFromString = ipAddress.instance(string);
+        const ipv4 = '127.0.0.1';
+        const addressFromString = ipAddress.instance(ipv4);
         it('should accept string ipv4 address', function() {
-            assert.equal(addressFromString.string, string);
+            assert.equal(addressFromString.type, 'IPv4');
+            assert.equal(addressFromString.string, ipv4);
         });
 
         const integer = 2130706433;
         let addressFromInteger = ipAddress.instance(integer);
         it('should accept integer representation of ipv4 address', function() {
-            assert.equal(addressFromInteger.string, string);
+            assert.equal(addressFromString.type, 'IPv4');
+            assert.equal(addressFromInteger.string, ipv4);
         });
 
         const largeInteger = 4294967296;
-
         it('should not accept large integer representation of ipv4 address', function() {
             assert.throws(() => ipAddress.instance(largeInteger));
+        });
+
+        const ipv6Short = '2001:db8::8a2e:370:7334';
+        it('should accept short string ipv6 address', function() {
+            const addressFromString = ipAddress.instance(ipv6Short);
+            assert.equal(addressFromString.type, 'IPv6');
+            assert.equal(addressFromString.string, ipv6Short);
+        });
+
+        const ipv6Long = '2001:0db8:0000:0000:0000:8a2e:0370:7334';
+        it('should accept long string ipv6 address', function() {
+            const addressFromString = ipAddress.instance(ipv6Long);
+            assert.equal(addressFromString.type, 'IPv6');
+            assert.equal(addressFromString.string, ipv6Long);
         });
     });
 
