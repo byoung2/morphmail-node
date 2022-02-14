4295f33fd588f066a96e2dd7edbe587976938c61
Bryan Young <byoung2@gmail.com>
2022-02-14 13:59:30 -0800

    Improvement: update execution scope for RPC

--- test/lib/server/rpc.test.js
+++ test/lib/server/rpc.test.js
@@ -4,7 +4,10 @@ const assert = require('assert');
 
 describe('RPC', function() {
     describe('methods', function() {
-        const rpc = RPC.instance();
+        const rpc = RPC.instance({
+            serverId: '1P6P52vQxPFnTiWC6jETWPHsnbrQAeca5P',
+            type: 'full',
+        });
         it('should call allowed method', async function() {
             const testWallet = '1HUqrQ9gkyDmkxnJ1TwSi1AbTncAfGGthM';
             const res = await rpc.call('registerPeer', [testWallet, 'client']);
@@ -15,7 +18,7 @@ describe('RPC', function() {
 
         it('should return string data', async function() {
             const res = await rpc.call('getServerId', []);
-            assert.equal(res.length, 34);
+            assert.equal(res, '1P6P52vQxPFnTiWC6jETWPHsnbrQAeca5P');
         });
 
         it('should throw an error on invalid method', async function() {
